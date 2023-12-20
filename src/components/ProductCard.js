43460cf503a1f2c4076c4068a6ceee6221e2783d
Precious Eruteya <eruteyaprecious@gmail.com>
2023-12-20 12:13:21 +0100

    Integrating Context and Reducer

---
+++ src/components/ProductCard.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useCart } from "../context/CartContext";
+import "./ProductCard.css"
+
+export const ProductCard = ({product}) => {
+  const { addToCart, removeFromCart, cartList } = useCart();
+  const [isInCart, setIsInCart] = useState(false);
+  
+  const {id, name, price, image} = product;
+
+  useEffect(() => {
+    const productIsInCart = cartList.find(cartItem => cartItem.id === id);
+
+    if(productIsInCart) {
+      setIsInCart(true)
+    } else {
+      setIsInCart(false);
+    }
+
+  }, [cartList, id])
+
+  // function handleAdd() {
+  //   addToCart(product);
+  //   console.log(cartList);
+  // }
+
+  return (
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>${price}</p>
+        {
+          isInCart ? ( <button className="remove" onClick={() => removeFromCart(product)}>Remove</button>) :   <button onClick={() => addToCart(product)}>Add To Cart</button>
+        }
+      </div>
+    </div>
+  )
+}
